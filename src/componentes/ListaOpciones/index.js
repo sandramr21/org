4db9435aeffcd188135acfa9988dd225597b1cc2
Sandra <sandramr21@gmail.com>
2023-08-16 17:19:20 -0500

    Clase React completada

---
+++ src/componentes/ListaOpciones/index.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./ListaOpciones.css";
+
+const ListaOpciones = (props) => {
+  //metodo map -> arreglo.map ((equipo, index)=>{
+  //return <option></option>
+  //})
+
+  const manejarCambio = (e) => {
+    console.log("cambio", e.target.value);
+    props.actualizarEquipo(e.target.value);
+  };
+
+  return (
+    <div className="lista-opciones">
+      <label>Equipos</label>
+      <select value={props.valor} onChange={manejarCambio}>
+        <option value="" disable defaultValue="" hidden>
+          Seleccionar equipo
+        </option>
+        {props.equipos.map((equipo, index) => (
+          <option key={index} value={equipo}>
+            {equipo}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default ListaOpciones;
